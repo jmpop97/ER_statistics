03819d1754a592ffdec619c0c4d48d80a1d49af4
jmpop <pjm970128@gmail.com>
2023-09-13 09:27:44 +0900

    add ER_apis

---
+++ ER_apis/ER_api.py
@@ -0,0 +1,33 @@
+import requests
+import json
+import os
+import time
+
+# Setting Header
+with open("secret.json", "r", encoding='utf-8') as f:
+    token = json.load(f)
+headerDict = {}
+headerDict.setdefault('x-api-key', token["token"])
+paramDict = {}
+
+def game_api(game_id): 
+    requestDataWithHeader = requests.get(
+        f'https://open-api.bser.io/v1/games/{game_id}', headers=headerDict)
+    responce_datas = requestDataWithHeader.json()
+    save_game(game_id,responce_datas)
+
+def save_game(game_id,responce_datas):
+    with open(f"./datas/{game_id}.json",'w',encoding='utf-8') as outfile:
+        json.dump(responce_datas,outfile, indent="\t",ensure_ascii=False)
+
+def save_games(start_game,end_game):
+    game_id=start_game
+    while game_id<=end_game:
+        game_api(game_id)
+        print(game_id)
+        game_id+=1
+
+        
+
+
+
