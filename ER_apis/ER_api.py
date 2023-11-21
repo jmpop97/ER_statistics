8e4f943caf848a451037aca34ea4a0f6db67aa55
jmpop97 <99373073+jmpop97@users.noreply.github.com>
2023-11-21 21:26:55 +0900

    Update ER_api.py

--- ER_apis/ER_api.py
+++ ER_apis/ER_api.py
@@ -14,20 +14,17 @@ def game_api(game_id):
     requestDataWithHeader = requests.get(
         f'https://open-api.bser.io/v1/games/{game_id}', headers=headerDict)
     responce_datas = requestDataWithHeader.json()
-    while responce_datas['message'] != "Success":
-        print("too much request")
-        responce_datas = requestDataWithHeader.json()
     save_game(game_id,responce_datas)
-    return True
 
 def save_game(game_id,responce_datas):
     with open(f"./datas/{game_id}.json",'w',encoding='utf-8') as outfile:
         json.dump(responce_datas,outfile, indent="\t",ensure_ascii=False)
 
-def save_games(start_game,n):
+def save_games(start_game,n=1,second=1):
     game_id=start_game
     while game_id<start_game+n:
-        if game_api(game_id)==True:
-            print("saved", game_id)
-            game_id+=1
+        game_api(game_id)
+        print(game_id)
+        game_id+=1
+        time.sleep(second)
     print("end")
