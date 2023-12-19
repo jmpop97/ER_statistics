7b61b41462ee7fbb23f652522b5bf0cd08cd4be1
dbehdrnr0202 <bb06001@naver.com>
2023-12-19 22:29:26 +0900

    refactor: EC2 v1.0

--- ER_EC2/insert_mongoDB.py
+++ ER_EC2/insert_mongoDB.py
@@ -12,6 +12,12 @@ import argparse
 argument_parser = argparse.ArgumentParser()
 argument_parser.add_argument("--n", help="GameNumbersToSave", type=int, default=1000)
 
+# need to optimaze
+# 1. how to get from_game_id to save
+# 2. how often to get apis.(current: every 00:00)
+# 3. how many datas to get?(if storage can store more, then execute often?)
+# 3.1 if we fetch datas too often, then the ranker's recent game_id won't change.
+
 if __name__ == "__main__":
     highest_game_id_in_DB = get_highest_id()
     recent_game_id_from_top_ranker = get_recent_game_id_from_ranker()
@@ -19,8 +25,10 @@ if __name__ == "__main__":
     print("recent_game_id_from_top_ranker: ", recent_game_id_from_top_ranker)
     from_game_id = recent_game_id_from_top_ranker
     game_numbers_to_save = argument_parser.parse_args().n
+    
     # don't use added value
     # have to prevent duplicated key(game id) error
+    
     if highest_game_id_in_DB >= recent_game_id_from_top_ranker - game_numbers_to_save:
         # here
         game_numbers_to_save = (
@@ -30,6 +38,9 @@ if __name__ == "__main__":
     # just kill
     if highest_game_id_in_DB ==recent_game_id_from_top_ranker:
         exit(0)
+    
+    # what if recent_game_id_from_top_ranker value is too much higher than highest_game_id_in_DB ?
+    # then average value of both values?
     insert_game_play_datas_mongoDB(
         from_game_id=from_game_id, game_numbers_to_save=game_numbers_to_save
     )
--- ER_EC2/insert_to_mongoDB.sh
+++ ER_EC2/insert_to_mongoDB.sh
@@ -1,2 +1,11 @@
 INSERT_DOCUMENT_NUMBER = 1000
-python ./insert_mongoDB.py --n $INSERT_DOCUMENT_NUMBER
+DISK_LIMIT=90;
+DOCUMENT_DELETE_NUMBER=1000;
+
+# get disk_used by df command
+disk_used=$(df / | grep ^/ | awk '{print $5}');
+# substring $
+disk_used=${disk_used::-1};
+# if DISK_LIMIT > disk_used
+# then execute python ./insert_mongoDB.py $INSERT_DOCUMENT_NUMBER to save game match datas.
+test $DISK_LIMIT -gt $disk_used && python ./insert_mongoDB.py --n $INSERT_DOCUMENT_NUMBER
