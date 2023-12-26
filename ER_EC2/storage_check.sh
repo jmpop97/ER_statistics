0ee4d1a364ed899462a54ffebcfbe2e43b23bc11
dbehdrnr0202 <bb06001@naver.com>
2023-12-26 23:13:09 +0900

    fix: used absolute path and used nohup to execute in offline

--- ER_EC2/storage_check.sh
+++ ER_EC2/storage_check.sh
@@ -1,5 +1,6 @@
 # DISK LIMIT percentage
-DISK_LIMIT=90;
+CURRENT_PATH=$(pwd)
+DISK_LIMIT=98;
 DOCUMENT_DELETE_NUMBER=1000;
 
 # get disk_used by df command
@@ -8,4 +9,5 @@ disk_used=$(df / | grep ^/ | awk '{print $5}');
 disk_used=${disk_used::-1};
 # if DISK_LIMIT < disk_used
 # then execute python ./delete_old_files.py $DOCUMENT_DELETE_NUMBER
-test $DISK_LIMIT -lt $disk_used && python ./delete_old_files.py $DOCUMENT_DELETE_NUMBER
+# absolute path
+test $DISK_LIMIT -lt $disk_used && nohup python ${CURRENT_PATH}/delete_old_files.py $DOCUMENT_DELETE_NUMBER &
