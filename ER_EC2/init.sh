6f5264b2a1fc6f74ffd7eb155a93154dabe398c6
dbehdrnr0202 <bb06001@naver.com>
2023-12-27 14:25:12 +0900

    fix

--- ER_EC2/init.sh
+++ ER_EC2/init.sh
@@ -1,19 +1,17 @@
-CURRENT_PATH=$(pwd)
+#!/bin/bash
 
 #if you already installed postfix
-sudo apt-get install postfix -y
-
+#sudo apt-get install postfix -y
+ER_DIR_PATH=$(pwd)
 sudo chmod +x ./storage_check.sh ./insert_to_mongoDB.sh
-echo "set file permissions ended"
-
 crontab -l > cron_backup
 # added environment path
 echo "SHELL=/bin/bash" >> cron_backup
 echo "PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin" >> cron_backup
+echo "ER_DIR_PATH=${ER_DIR_PATH}" >> cron_backup
 # minute, hour, day, month, day of the week
 # storage check every 30 minutes
-echo "*/30 * * * * ${CURRENT_PATH}/storage_check.sh >> ${CURRENT_PATH}/logs/storage_check.log 2>&1" >> cron_backup
+echo "*/30 * * * * ${ER_DIR_PATH}/storage_check.sh >> ${ER_DIR_PATH}/logs/storage_check.log 2>&1" >> cron_backup
 # insert game match datas every day 00:00
-echo "0 0 * * * ${CURRENT_PATH}/insert_to_mongoDB.sh >> ${CURRENT_PATH}/logs/insert_to_mongoDB.log 2>&1" >> cron_backup
+echo "0 0 * * * ${ER_DIR_PATH}/insert_to_mongoDB.sh >> ${ER_DIR_PATH}/logs/insert_to_mongoDB.log 2>&1" >> cron_backup
 crontab cron_backup
-echo "crontab edit ended"
