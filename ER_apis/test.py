15e552f60072d93f1dc306107bdb45534fe6611d
dbehdrnr0202 <bb06001@naver.com>
2024-01-19 22:28:18 +0900

    style: removed caption

--- ER_apis/test.py
+++ ER_apis/test.py
@@ -11,12 +11,10 @@ import unittest
 
 class SaveGames(unittest.TestCase):
     # ????
-    """
     def test_save_game(self):
         with open("./datas/Ver9.0_Rank_31131392.json", "r", encoding="utf-8") as f:
             game_datas = json.load(f)
         self.assertTrue(game_datas.get("code", 0) == 200)
-    """
 
     def test_request_to_ER_api(self):
         save_result = request_to_ER_api(
