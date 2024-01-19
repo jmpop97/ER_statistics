5af585d03cc025af4f3a55b966449694f1decc1d
dbehdrnr0202 <bb06001@naver.com>
2024-01-19 21:32:55 +0900

    test: added test code for request api

--- ER_apis/test.py
+++ ER_apis/test.py
@@ -1,6 +1,6 @@
 import json
 
-from .ER_api import save_games
+from .ER_api import save_games, request_free_characters
 
 import unittest
 
@@ -9,3 +9,5 @@ class SaveGames(unittest.TestCase):
         with open("./datas/Ver9.0_Rank_31131392.json", "r", encoding="utf-8") as f:
             game_datas = json.load(f)
         self.assertTrue(game_datas.get("code", 0) == 200)
+    def test_request_free_characters(self):
+        self.assertTrue(request_free_characters())
