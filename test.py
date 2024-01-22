41cda9d9d2da625266f06d5d77e55e784ad03341
jmpop <pjm970128@gmail.com>
2024-01-22 21:33:33 +0900

    test

--- test.py
+++ test.py
@@ -5,8 +5,8 @@ import unittest
 class TestForeignTeam(unittest.TestCase):
     def test_true(self):
         self.assertEqual(1,1)
-    def test_fail(self):
-        self.assertEqual("change",2)
+    # def test_fail(self):
+    #     self.assertEqual("change",2)
 
 if __name__ == "__main__":
     unittest.main()
