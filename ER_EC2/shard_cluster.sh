efe2c6d2299130d09ae5df9b9ad459685b7af3cb
dbehdrnr0202 <bb06001@naver.com>
2024-01-23 21:27:27 +0900

    feat: added shard cluster.sh code

--- ER_EC2/shard_cluster.sh
+++ ER_EC2/shard_cluster.sh
@@ -1,15 +1,20 @@
-# ec2 inbound rule 처리
-# cluster 내 ec2를 같은 보안 그룹으로 처리
-
 #!/bin/bash
 status=$(sudo systemctl status mongod | grep "Active" | awk '{print $2}')
 echo $status
-: << "END"
-set_name=$2
-
+if [ $status -ne "active" ]
+then
+  echo "systemctl status mongod is not working"
+  exit(1)
+fi
+set_name=$1
+host_ip_1=$2
+host_ip_2=$3
+host_ip_3=$4
 mongo_conf="/etc/mongod.conf"
 db_path=$(grep "dbPath" $mongo_conf | awk '$2')
+port=$(grep "port" $mongo_conf | awk '$2')
 echo $db_path
+echo $port
 cp $mongo_conf $db_path/mongod_backup.conf
 cat > $mongo_conf <<EOF
 # mongod.conf
@@ -37,46 +42,25 @@ security:
   keyFile: ${db_path}/mongodb.key
 EOF
 
-
 openssl rand -base64 741 > ${dbPath}/mongodb.key
 chmod 600 mongodb.key
-
 sudo systemctl stop mongod
 sudo systemctl start mongod
-
-sudo systemctl stop mongod
-sudo systemctl start mongod
-status=$(grep "Active" sudo systemctl status mongod | awk '$2')
-
-mongosh << EOF
-rs.status
-exit
+status=$(sudo systemctl status mongod | grep "Active" | awk '{print $2}')
+port=$(grep "port: " $mongo_conf | awk '$2')
+port=${port:8}echo $status
+if [ $status == "active" ]
+then
+  mongosh << EOF
+  rs.initiate({
+      _id : $set_name,
+      members : [
+          {_id : 0, host : $host_ip_1:$port},
+          {_id : 1, host : $host_ip_2:$port},
+          {_id : 2, host : $host_ip_3:$port},
+      ]
+  })
+  rs.status
+  exit
 EOF
-
-# /etc/mongod.conf
-storage:
-    dbPath: /root/
-replication:
-    replSetName: "set_name"
-security:
-    authorization: enabled
-    keyFile: ${dbPath}/mongodb.key
-
-openssl rand -base64 741 > ${dbPath}/mongodb.key
-chmod 600 mongodb.key
-
-
-sudo systemctl stop mongod
-sudo systemctl start mongod
-sudo systemctl status mongod 
-
-mongosh
-echo "rs.initiate({
-    _id : \"set_name\",
-    members : [
-        {_id : 0, host : \"<BindIp>:<Port>\"},
-        {_id : 1, host : \"<BindIp>:<Port>\"},
-        {_id : 2, host : \"<BindIp>:<Port>\"},
-    ]
-})"
-rs.status
+fi
